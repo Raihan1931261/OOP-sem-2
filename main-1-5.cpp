6393ccf436e28ecbee08365c36ec6ffb952c0e8e
Raihan1931261 <a1931261@adelaide.edu.au>
2025-08-03 20:40:41 +0930

    Create main-1-5.cpp

---
+++ main-1-5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int count_evens(int number);
+
+int main() {
+    int num = 10;
+    cout << "Even numbers count from 1 to 10: " << count_evens(num) << endl;
+    return 0;
+}
