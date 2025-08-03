6bef45506fab7710bcd819b893da24a69c738d38
Raihan1931261 <a1931261@adelaide.edu.au>
2025-08-03 21:03:30 +0930

    Update function-3-3.cpp

--- function-3-3.cpp
+++ function-3-3.cpp
@@ -1,26 +1,12 @@
-#include <iostream>
-#include <map>
-using namespace std;
-
 double weighted_average(int array[], int n) {
-    // Return 0 if size is less than 1
-    if (n < 1) {
-        return 0.0;
-    }
-    
-    // Count frequency of each element
-    map<int, int> frequency;
+    if (n < 1) return 0;
+    double sum = 0;
     for (int i = 0; i < n; i++) {
-        frequency[array[i]]++;
+        int freq = 0;
+        for (int j = 0; j < n; j++) {
+            if (array[j] == array[i]) freq++;
+        }
+        sum += array[i] * ((double)freq / n);
     }
-    
-    // Calculate weighted average
-    double weighted_sum = 0.0;
-    for (map<int, int>::iterator it = frequency.begin(); it != frequency.end(); ++it) {
-        int element = it->first;
-        int freq = it->second;
-        weighted_sum += element * (double(freq) / n);
-    }
-    
-    return weighted_sum;
+    return sum;
 }
