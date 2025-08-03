ce4e69a4d90d06acc8766d368d7d131293b8e335
Raihan1931261 <a1931261@adelaide.edu.au>
2025-08-03 20:45:08 +0930

    Create main-2-3.cpp

---
+++ main-2-3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+void two_five_nine(int array[], int n);
+
+int main() {
+    int arr[10] = {2, 5, 9, 9, 2, 3, 9, 2, 1, 5};
+    two_five_nine(arr, 10);
+    return 0;
+}
