4706228c3e24b774db2a2ce3562e18f889ead228
Raihan1931261 <a1931261@adelaide.edu.au>
2025-08-03 20:55:41 +0930

    Create main-3-4.cpp

---
+++ main-3-4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+void print_pass_fail(char grade);
+
+int main() {
+    print_pass_fail('A');
+    print_pass_fail('D');
+    print_pass_fail('X');
+    return 0;
+}
