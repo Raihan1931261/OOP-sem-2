0033ea9d5eeb2f24edea37201f8f8afa5d1e074c
Raihan1931261 <a1931261@adelaide.edu.au>
2025-08-06 22:28:17 +0930

    Update function-1-3.cpp

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
-using namespace std;
 
 void count_digits(int array[4][4]) {
-    int count[10] = {0};
-
-    // Count digits
+    int count[10] = {0}; // Array to store count of each digit 0-9
+    
+    // Count occurrences of each digit
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
-            int val = array[i][j];
-            if (val >= 0 && val <= 9) {
-                count[val]++;
+            if (array[i][j] >= 0 && array[i][j] <= 9) {
+                count[array[i][j]]++;
             }
         }
     }
-
-    for (int i = 0; i < 10; i++) {
-        cout << i << ":" << count[i];
-        if (i < 9) cout << ";";
-    }
-    cout << endl;
+    
+    // Print results
+    std::cout << "0:" << count[0] << ";1:" << count[1] << ";2:" << count[2] << ";3:" << count[3]
+              << ";4:" << count[4] << ";5:" << count[5] << ";6:" << count[6] << ";7:" << count[7]
+              << ";8:" << count[8] << ";9:" << count[9] << ";";
 }
