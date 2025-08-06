d3d170715dbe35195b378f8b525e53c98f2504ef
Raihan1931261 <a1931261@adelaide.edu.au>
2025-08-06 22:23:13 +0930

    Update function-1-3.cpp

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -7,4 +7,16 @@ void count_digits(int array[4][4]) {
     // Count digits
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
-            int val = array[i]
+            int val = array[i][j];
+            if (val >= 0 && val <= 9) {
+                count[val]++;
+            }
+        }
+    }
+
+    for (int i = 0; i < 10; i++) {
+        cout << i << ":" << count[i];
+        if (i < 9) cout << ";";
+    }
+    cout << endl;
+}
