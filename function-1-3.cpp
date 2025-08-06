426f0b3a3466f9c0bf97c96f741c9df73015cd31
Raihan1931261 <a1931261@adelaide.edu.au>
2025-08-06 22:15:03 +0930

    Create function-1-3.cpp

---
+++ function-1-3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void count_digits(int array[4][4]) {
+    int count[10] = {0};
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (array[i][j] >= 0 && array[i][j] <= 9) {
+                count[array[i][j]]++;
+            }
+        }
+    }
+
+    for (int i = 0; i < 10; i++) {
+        cout << i << ":" << count[i];
+        if (i < 9) cout << ";";
+    }
+    cout << endl;
+}
