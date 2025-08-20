48e8b79085e00a5735b6c097786080e0c1e19e40
Raihan1931261 <a1931261@adelaide.edu.au>
2025-08-20 23:22:11 +0930

    Create function-3-1.cpp

---
+++ function-3-1.cpp
@@ -0,0 +1,10 @@
+// function-3-1.cpp
+#include <iostream>
+using namespace std;
+
+bool equalsArray(int *numbers1, int *numbers2, int length) {
+    if (length < 1) return false;
+    for (int i = 0; i < length; ++i)
+        if (numbers1[i] != numbers2[i]) return false;
+    return true;
+}
