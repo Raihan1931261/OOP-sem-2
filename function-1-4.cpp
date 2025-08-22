85fe9b337b558973064bfdfb69f9550c841cf718
Raihan1931261 <a1931261@adelaide.edu.au>
2025-08-22 23:59:33 +0930

    Update function-1-4.cpp

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -1,8 +1,13 @@
 #include "Person.h"
 
 PersonList shallowCopyPersonList(PersonList pl) {
-    PersonList copy;
-    copy.numPeople = pl.numPeople;
-    copy.people = pl.people; // points to the same array
-    return copy;
+    PersonList out;
+    if (pl.numPeople <= 0 || pl.people == nullptr) {
+        out.people = nullptr;
+        out.numPeople = 0;
+        return out;
+    }
+    out.people = pl.people;      // alias the same array
+    out.numPeople = pl.numPeople;
+    return out;
 }
