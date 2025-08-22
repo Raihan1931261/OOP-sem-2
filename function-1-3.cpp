8373faf96097ce9658a232196464b5ef80d2daf5
Raihan1931261 <a1931261@adelaide.edu.au>
2025-08-22 23:58:35 +0930

    Update function-1-3.cpp

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -1,11 +1,20 @@
 #include "Person.h"
 
+
 PersonList deepCopyPersonList(PersonList pl) {
-    PersonList copy;
-    copy.numPeople = pl.numPeople;
-    if (copy.numPeople > 0 && pl.people) {
-        copy.people = new Person[copy.numPeople];
-        for (int i = 0; i < copy.numPeople; ++i) copy.people[i] = pl.people[i];
+    PersonList out;
+    // Treat any non-sensical input as empty.
+    if (pl.numPeople <= 0 || pl.people == nullptr) {
+        out.people = nullptr;
+        out.numPeople = 0;
+        return out;
     }
-    return copy;
+
+    out.numPeople = pl.numPeople;
+    out.people = new Person[out.numPeople];
+    for (int i = 0; i < out.numPeople; ++i) {
+        out.people[i].name = pl.people[i].name;
+        out.people[i].age  = pl.people[i].age;
+    }
+    return out;
 }
