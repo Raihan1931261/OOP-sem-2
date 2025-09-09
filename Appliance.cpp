aeeadd24254e6d5a62a94d55f2014ceadeb3a3ca
Raihan1931261 <a1931261@adelaide.edu.au>
2025-09-09 17:21:05 +0930

    Update Appliance.cpp

--- Appliance.cpp
+++ Appliance.cpp
@@ -1,39 +1,48 @@
 #include "Appliance.h"
 
+// default constructor
 Appliance::Appliance() {
-    powerRating = 0;
-    isOn = false;
+    powerRating = 0; // set to 0
+    isOn = false; // set to false
 }
 
+// constructor with parameter
 Appliance::Appliance(int powerRating) {
     this->powerRating = powerRating;
     isOn = false;
 }
 
+// get power rating
 int Appliance::get_powerRating() {
     return powerRating;
 }
 
+// get if on
 bool Appliance::get_isOn() {
     return isOn;
 }
 
+// set power rating
 void Appliance::set_powerRating(int powerRating) {
     this->powerRating = powerRating;
 }
 
+// set on/off
 void Appliance::set_isOn(bool isOn) {
     this->isOn = isOn;
 }
 
+// turn on appliance
 void Appliance::turnOn() {
     isOn = true;
 }
 
+// turn off appliance
 void Appliance::turnOff() {
     isOn = false;
 }
 
+// return power consumption - base returns 0
 double Appliance::getPowerConsumption() {
     return 0;
 }
