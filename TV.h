67f9681cd3fede3c18d34ff32041b482f968036b
Raihan1931261 <a1931261@adelaide.edu.au>
2025-09-09 17:26:58 +0930

    Update TV.h

--- TV.h
+++ TV.h
@@ -3,17 +3,18 @@
 
 #include "Appliance.h"
 
+// TV class inherits from Appliance
 class TV : public Appliance {
 private:
-    double screenSize;
+    double screenSize; // screen size in inches
 
 public:
-    TV();
-    TV(int powerRating, double screenSize);
+    TV(); // default constructor
+    TV(int powerRating, double screenSize); // constructor with parameters
     
-    void setScreenSize(double screenSize);
-    double getScreenSize();
-    double getPowerConsumption();
+    void setScreenSize(double screenSize); // set screen size
+    double getScreenSize(); // get screen size
+    double getPowerConsumption(); // calculate power consumption
 };
 
 #endif
