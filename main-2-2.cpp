62265dbcd3c791eed6d33bace02f57a8c19cd0df
Raihan1931261 <a1931261@adelaide.edu.au>
2025-09-09 17:29:18 +0930

    Update main-2-2.cpp

--- main-2-2.cpp
+++ main-2-2.cpp
@@ -3,19 +3,23 @@
 using namespace std;
 
 int main() {
+    // test default constructor
     TV tv1;
     cout << "TV power: " << tv1.get_powerRating() << endl;
     cout << "TV size: " << tv1.getScreenSize() << endl;
     
+    // test constructor with parameters
     TV tv2(200, 55);
     cout << "TV2 power: " << tv2.get_powerRating() << endl;
     cout << "TV2 size: " << tv2.getScreenSize() << endl;
     cout << "TV2 consumption: " << tv2.getPowerConsumption() << endl;
     
+    // test setters and methods
     tv1.set_powerRating(180);
     tv1.setScreenSize(42);
     cout << "TV1 consumption: " << tv1.getPowerConsumption() << endl;
     
+    // test turning on
     tv2.turnOn();
     cout << "TV2 on: " << tv2.get_isOn() << endl;
     
