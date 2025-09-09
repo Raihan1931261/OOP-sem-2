c9b15ad0f5f4135378e55f812522174962e9050b
Raihan1931261 <a1931261@adelaide.edu.au>
2025-09-09 17:28:48 +0930

    Update main-2-1.cpp

--- main-2-1.cpp
+++ main-2-1.cpp
@@ -3,15 +3,18 @@
 using namespace std;
 
 int main() {
+    // test default constructor
     Fridge f1;
     cout << "Fridge power: " << f1.get_powerRating() << endl;
     cout << "Fridge volume: " << f1.getVolume() << endl;
     
+    // test constructor with parameters
     Fridge f2(150, 200);
     cout << "F2 power: " << f2.get_powerRating() << endl;
     cout << "F2 volume: " << f2.getVolume() << endl;
     cout << "F2 consumption: " << f2.getPowerConsumption() << endl;
     
+    // test setters
     f1.set_powerRating(120);
     f1.setVolume(180);
     cout << "F1 new power: " << f1.get_powerRating() << endl;
