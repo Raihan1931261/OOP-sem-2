224a708a0a299ea6fce80ed689059e43e0233a9c
Raihan1931261 <a1931261@adelaide.edu.au>
2025-09-09 17:19:55 +0930

    Update Appliance.h

--- Appliance.h
+++ Appliance.h
@@ -1,23 +1,27 @@
 #ifndef APPLIANCE_H
 #define APPLIANCE_H
 
+// Base class for appliances
 class Appliance {
 private:
-    int powerRating;
-    bool isOn;
+    int powerRating; // power rating
+    bool isOn; // if appliance is on
 
 public:
+    // constructors
     Appliance();
     Appliance(int powerRating);
     
+    // getters
     int get_powerRating();
     bool get_isOn();
+    // setters
     void set_powerRating(int powerRating);
     void set_isOn(bool isOn);
     
     void turnOn();
     void turnOff();
-    virtual double getPowerConsumption();
+    virtual double getPowerConsumption(); // virtual function
 };
 
 #endif
