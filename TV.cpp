870b438e240563bd56860e986c621c2568edb3c1
Raihan1931261 <a1931261@adelaide.edu.au>
2025-09-09 17:27:47 +0930

    Update TV.cpp

--- TV.cpp
+++ TV.cpp
@@ -1,22 +1,27 @@
 #include "TV.h"
 
+// default constructor
 TV::TV() {
-    screenSize = 0;
+    screenSize = 0; // set screen size to 0
 }
 
+// constructor with parameters
 TV::TV(int powerRating, double screenSize) : Appliance(powerRating) {
-    this->screenSize = screenSize;
+    this->screenSize = screenSize; // set screen size
 }
 
+// set the screen size
 void TV::setScreenSize(double screenSize) {
     this->screenSize = screenSize;
 }
 
+// get the screen size
 double TV::getScreenSize() {
     return screenSize;
 }
 
+// calculate power consumption using the formula
 double TV::getPowerConsumption() {
-    double power = get_powerRating() * (screenSize / 10.0);
+    double power = get_powerRating() * (screenSize / 10.0); // formula from assignment
     return power;
 }
