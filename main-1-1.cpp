dfeb6ccac02e6e3fed7a3b668e51aa16b3f344f8
Raihan1931261 <a1931261@adelaide.edu.au>
2025-09-09 17:25:23 +0930

    Update main-1-1.cpp

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -3,19 +3,23 @@
 using namespace std;
 
 int main() {
+    // test default constructor
     Appliance app1;
     cout << "Power rating: " << app1.get_powerRating() << endl;
     cout << "Is on: " << app1.get_isOn() << endl;
     
+    // test constructor with parameter
     Appliance app2(100);
     cout << "App2 power: " << app2.get_powerRating() << endl;
     
+    // test turn on/off
     app2.turnOn();
     cout << "App2 is on now: " << app2.get_isOn() << endl;
     
     app2.turnOff();
     cout << "App2 is on: " << app2.get_isOn() << endl;
     
+    // test power consumption
     cout << "Power consumption: " << app2.getPowerConsumption() << endl;
     
     return 0;
