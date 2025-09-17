2bf13ccbdc8825f1730e1b72bf7b3ff4a521756f
Raihan1931261 <a1931261@adelaide.edu.au>
2025-09-17 21:20:46 +0930

    Update Addition.h

--- Addition.h
+++ Addition.h
@@ -1,5 +1,7 @@
+// A simple class that provides an add function
 class Addition {
 public:
+    // Function that returns the sum of two integers
     int add(int lhs, int rhs) {
         return lhs + rhs;
     }
