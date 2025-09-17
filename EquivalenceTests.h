06873d7f48e8fe31202b0b405c7aade345faaa0c
Raihan1931261 <a1931261@adelaide.edu.au>
2025-09-17 21:29:31 +0930

    Update EquivalenceTests.h

--- EquivalenceTests.h
+++ EquivalenceTests.h
@@ -1,37 +1,12 @@
+#pragma once
 #include <iostream>
 #include "Addition.h"
 
-// A class to test different input categories (equivalence classes)
 class EquivalenceTests {
 public:
-    void runTests() {
-        testPositiveIntegers();
-        testNegativeIntegers();
-        testMixedNumbers();
-    }
-
+    void runTests() { pos(); neg(); mixed(); }
 private:
-    // Positive numbers test
-    void testPositiveIntegers() {
-        Addition addition;
-        if (addition.add(10, 5) != 15) {
-            std::cout << "Positive integers test failed!" << std::endl;
-        }
-    }
-
-    // Negative numbers test
-    void testNegativeIntegers() {
-        Addition addition;
-        if (addition.add(-4, -5) != -9) {
-            std::cout << "Negative integers test failed!" << std::endl;
-        }
-    }
-
-    // One negative, one positive
-    void testMixedNumbers() {
-        Addition addition;
-        if (addition.add(-3, 7) != 4) {
-            std::cout << "Mixed numbers test failed!" << std::endl;
-        }
-    }
+    void pos()   { Addition a; if (a.add(10,5)  != 15) std::cout << "pos failed\n"; }
+    void neg()   { Addition a; if (a.add(-4,-5) != -9) std::cout << "neg failed\n"; }
+    void mixed() { Addition a; if (a.add(-3,7)  != 4)  std::cout << "mixed failed\n"; }
 };
