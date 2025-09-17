967b1489e5788609bc8f6693aed4ac0769a407ae
Raihan1931261 <a1931261@adelaide.edu.au>
2025-09-17 21:25:13 +0930

    Update Invoice.h

--- Invoice.h
+++ Invoice.h
@@ -1,29 +1,13 @@
 #include <string>
 
-// Class representing a billing invoice
 class Invoice {
 private:
-    std::string invoiceId;   // Unique invoice ID
-    double dollarsOwed;      // Amount owed in dollars
+    std::string invoiceId;
+    double dollarsOwed;
 
 public:
-    // Constructor initializes invoice with an ID
-    Invoice(std::string id) : invoiceId(id), dollarsOwed(0) {}
-
-    // Adds service cost to the invoice (only positive values allowed)
-    void addServiceCost(double costDollars) {
-        if (costDollars > 0) {
-            dollarsOwed += costDollars;
-        }
-    }
-
-    // Returns the total amount owed
-    double getDollarsOwed() {
-        return dollarsOwed;
-    }
-
-    // Returns the invoice ID
-    std::string getInvoiceId() {
-        return invoiceId;
-    }
+    Invoice(std::string id); // constructor only declared
+    void addServiceCost(double costDollars);
+    double getDollarsOwed();
+    std::string getInvoiceId();
 };
