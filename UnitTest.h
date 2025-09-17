ac8890f9ebed944f0db6b2fbf3ffe02207412670
Raihan1931261 <a1931261@adelaide.edu.au>
2025-09-17 21:29:10 +0930

    Update UnitTest.h

--- UnitTest.h
+++ UnitTest.h
@@ -1,29 +1,15 @@
+#pragma once
 #include <iostream>
 #include "Addition.h"
 
-// A test class to check whether Addition class works correctly
 class UnitTest {
 public:
-    // Public function that runs all test cases
-    void runTests() {
-        testAddition();
-    }
-
+    void runTests() { testAddition(); }
 private:
-    // Private function to test Addition::add
     void testAddition() {
-        Addition addition;
-
-        if (addition.add(1, 2) != 3) {
-            std::cout << "Test 1 failed!" << std::endl;
-        }
-
-        if (addition.add(-1, -1) != -2) {
-            std::cout << "Test 2 failed!" << std::endl;
-        }
-
-        if (addition.add(0, 5) != 5) {
-            std::cout << "Test 3 failed!" << std::endl;
-        }
+        Addition a;
+        if (a.add(1,2) != 3) std::cout << "Test 1 failed\n";
+        if (a.add(-1,-1) != -2) std::cout << "Test 2 failed\n";
+        if (a.add(0,5) != 5) std::cout << "Test 3 failed\n";
     }
 };
