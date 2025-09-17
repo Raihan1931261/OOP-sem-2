73e909dcb06aef27c0f24d136c25a5edf6faef9b
Raihan1931261 <a1931261@adelaide.edu.au>
2025-09-17 21:30:13 +0930

    Update Invoice.h

--- Invoice.h
+++ Invoice.h
@@ -1,12 +1,12 @@
+#pragma once
 #include <string>
 
 class Invoice {
 private:
     std::string invoiceId;
     double dollarsOwed;
-
 public:
-    Invoice(std::string id); // constructor only declared
+    Invoice(std::string id);
     void addServiceCost(double costDollars);
     double getDollarsOwed();
     std::string getInvoiceId();
