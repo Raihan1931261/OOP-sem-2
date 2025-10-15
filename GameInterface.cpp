4e3e12ab1782e061054152d98492343e764fb9b2
Raihan1931261 <a1931261@adelaide.edu.au>
2025-10-15 17:44:57 +1030

    Create GameInterface.cpp

---
+++ GameInterface.cpp
@@ -0,0 +1,21 @@
+#ifndef GAMEINTERFACE_H
+#define GAMEINTERFACE_H
+
+#include "BattleGame.h"
+#include <iostream>
+
+/** Utility class: all methods are static (no object needed). */
+class GameInterface {
+public:
+    static void       displayWelcome();
+    static void       displayMenu();
+    static ActionType getPlayerAction(std::string playerName);
+    static void       displayTurnResult(std::string p1Name, ActionType p1Move,
+                                        std::string p2Name, ActionType p2Move);
+    static void       displayWinner(GameState state, std::string p1Name, std::string p2Name);
+    static bool       askPlayAgain();
+private:
+    static std::string actionTypeToString(ActionType action);
+};
+
+#endif
